0bb23fe74e8c14c5ee9b0a6eadd322f5816a3976
Mir, A <mir-am@hotmail.com>
2019-03-20 21:34:21 +0330

    Added a dataset and also LSTSVM code to repo.

---
+++ src/estimators.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Developers: Mir, A. and Mahdi Rahbar
+# Version: 0.1 - 2019-03-20
+# License:
+
+"""
+In this module, Standard TwinSVM and Least Squares TwinSVM estimators are defined.
+"""
+
+
+# TwinSVM code
+
+
+
+
+# End of TwinSVM code
+
+# Least Squares TwinSVM
+class LSTSVM:
+
+	"""
+	Least Squares Twin Support Vector Machine (LSTSVM) for binary classification
+	
+	Parameters
+	----------
+	
+	
+	Attributes
+	----------
+	
+	"""
+	
+	def __init__(self):
+	
+		pass
+		
+		
+	def fit(self):
+	
+		"""
+		"""
+		
+		pass
+		
+	def predict(self):
+	
+		"""
+		"""
+		
+		pass
+	
