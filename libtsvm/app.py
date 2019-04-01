f70d89667f5f1835948de7733d84f374096446dc
Mir, A <mir-am@hotmail.com>
2019-04-01 23:06:39 +0430

    Added file dialog for importing dataset.

--- libtsvm/app.py
+++ libtsvm/app.py
@@ -4,22 +4,45 @@
 # Version: 0.1 - 2019-03-20
 # License: GNU General Public License v3.0
 
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from libtsvm.ui import view
 import sys
 
 
-class LIBTwinSVMApp(view.Ui_MainWindow, QtWidgets.QMainWindow):
+class LIBTwinSVMApp(view.Ui_MainWindow, QMainWindow):
     
     def __init__(self):
         
         super(LIBTwinSVMApp, self).__init__()
+        
         self.setupUi(self)
+        self.init_GUI()
+        
+    def init_GUI(self):
+        """
+        Initialize the GUI of application
+        """
+        
+        self.open_btn.clicked.connect(self.import_data)
+        
+        
+    def import_data(self):
+        """
+        Handels dataset import
+        """
+        
+        data_filename, _ = QFileDialog.getOpenFileName(self, "Import dataset",
+                                                       "", "CSV files (*.csv)")
+        
+        if data_filename:
+            
+            self.path_box.setText(data_filename)
+
      
         
 def main():
     
-   app = QtWidgets.QApplication(sys.argv)
+   app = QApplication(sys.argv)
    libtsvm_app = LIBTwinSVMApp()
    libtsvm_app.show()
    sys.exit(app.exec_())
