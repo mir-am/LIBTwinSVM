df46a966fd283740b46d74660d18aa6ac33cebf2
mir-am <mir-am@hotmail.com>
2019-05-15 11:07:20 +0430

    Removed the check for GPU support.

--- libtsvm/estimators.py
+++ libtsvm/estimators.py
@@ -136,8 +136,6 @@ class BaseTSVM(BaseEstimator):
 #        kernel_f = {'linear': lambda i: X[i, :],
 #                    'RBF': lambda i: rbf_kernel(X[i, :], self.mat_C_t, self.gamma)}
 #        
-##         TODO: prediction can be sped up using NumPy's np.apply?!. It removes
-##         below for loop.
 #        for i in range(X.shape[0]):
 #
 #            # Prependicular distance of data pint i from hyperplanes
@@ -436,199 +434,200 @@ def rbf_kernel(x, y, u):
     return np.exp(-2 * u) * np.exp(2 * u * np.dot(x, y))
 
 # GPU implementation of estimators ############################################
-
-try:
-
-    from cupy import prof
-    import cupy as cp
-    
-    def GPU_rbf_kernel(x, y, u):
-        """
-        It transforms samples into higher dimension using Gaussian (RBF) kernel.
-    
-        Parameters
-        ----------
-        x, y : array-like, shape (n_features,)
-            A feature vector or sample.
-    
-        u : float
-            Parameter of the RBF kernel function.
-    
-        Returns
-        -------
-        float
-            Value of kernel matrix for feature vector x and y.
-        """
-    
-        return cp.exp(-2 * u) * cp.exp(2 * u * cp.dot(x, y))
-        
-    class GPU_LSTSVM():
-        """
-        GPU implementation of least squares twin support vector machine
-        
-        Parameters
-        ----------
-        kernel : str 
-            Type of the kernel function which is either 'linear' or 'RBF'.
-    
-        rect_kernel : float
-            Percentage of training samples for Rectangular kernel.
-    
-        C1 : float
-            Penalty parameter of first optimization problem.
-    
-        C2 : float
-            Penalty parameter of second optimization problem.
-    
-        gamma : float
-            Parameter of the RBF kernel function.
-    
-        Attributes
-        ----------
-        mat_C_t : array-like, shape = [n_samples, n_samples]
-            A matrix that contains kernel values.
-    
-        cls_name : str
-            Name of the classifier.
-    
-        w1 : array-like, shape=[n_features]
-            Weight vector of class +1's hyperplane.
-    
-        b1 : float
-            Bias of class +1's hyperplane.
-    
-        w2 : array-like, shape=[n_features]
-            Weight vector of class -1's hyperplane.
-    
-        b2 : float
-            Bias of class -1's hyperplane.
-        """
-        
-        def __init__(self, kernel='linear', rect_kernel=1, C1=2**0, C2=2**0,
-                     gamma=2**0):
-            
-            self.C1 = C1
-            self.C2 = C2
-            self.gamma = gamma
-            self.kernel = kernel
-            self.rect_kernel = rect_kernel
-            self.mat_C_t = None
-            self.clf_name = None
-            
-            # Two hyperplanes attributes
-            self.w1, self.b1, self.w2, self.b2 = None, None, None, None
-        
-        #@prof.TimeRangeDecorator()
-        def fit(self, X, y):
-            """
-            It fits the binary Least Squares TwinSVM model according to the given
-            training data.
-    
-            Parameters
-            ----------
-            X : array-like, shape (n_samples, n_features)
-                Training feature vectors, where n_samples is the number of samples
-                and n_features is the number of features.
-    
-            y : array-like, shape(n_samples,)
-                Target values or class labels.
-            """
-            
-            X = cp.asarray(X)
-            y = cp.asarray(y)
-            
-            # Matrix A or class 1 data
-            mat_A = X[y == 1]
-            
-            # Matrix B or class -1 data
-            mat_B = X[y == -1]
-            
-            # Vectors of ones
-            mat_e1 = cp.ones((mat_A.shape[0], 1))
-            mat_e2 = cp.ones((mat_B.shape[0], 1))
-            
-            mat_H = cp.column_stack((mat_A, mat_e1))
-            mat_G = cp.column_stack((mat_B, mat_e2))
-    
-            mat_H_t = cp.transpose(mat_H)
-            mat_G_t = cp.transpose(mat_G)
-            
-            # Determine parameters of two non-parallel hyperplanes
-            hyper_p_1 = -1 * cp.dot(cp.linalg.inv(cp.dot(mat_G_t, mat_G) + \
-                                   (1 / self.C1) * cp.dot(mat_H_t, mat_H)), \
-                                    cp.dot(mat_G_t, mat_e2))
-            
-            self.w1 = hyper_p_1[:hyper_p_1.shape[0] - 1, :]
-            self.b1 = hyper_p_1[-1, :]
-            
-            hyper_p_2 = cp.dot(cp.linalg.inv(cp.dot(mat_H_t, mat_H) + (1 / self.C2)
-                                                 * cp.dot(mat_G_t, mat_G)), cp.dot(mat_H_t, mat_e1))
-    
-            self.w2 = hyper_p_2[:hyper_p_2.shape[0] - 1, :]
-            self.b2 = hyper_p_2[-1, :]
-        
-        #@prof.TimeRangeDecorator()
-        def predict(self, X):
-            """
-            Performs classification on samples in X using the Least Squares 
-            TwinSVM model.
-            
-            Parameters
-            ----------
-            X_test : array-like, shape (n_samples, n_features)
-                Feature vectors of test data.
-                    
-            Returns
-            -------
-            output : array, shape (n_samples,)
-                Predicted class lables of test data.
-                
-            """
-            
-            X = cp.asarray(X)
-    
-            # Calculate prependicular distances for new data points 
-    #        prepen_distance = cp.zeros((X.shape[0], 2))
-    #
-    #        kernel_f = {'linear': lambda i: X[i, :] ,
-    #                    'RBF': lambda i: GPU_rbf_kernel(X[i, :], self.mat_C_t, self.gamma)}
-    #
-    #        for i in range(X.shape[0]):
-    #
-    #            # Prependicular distance of data pint i from hyperplanes
-    #            prepen_distance[i, 1] = cp.abs(cp.dot(kernel_f[self.kernel](i), self.w1) + self.b1)[0]
-    #        
-    #            prepen_distance[i, 0] = cp.abs(cp.dot(kernel_f[self.kernel](i), self.w2) + self.b2)[0]
-            
-            dist = cp.column_stack((cp.abs(cp.dot(X, self.w2) + self.b2), 
-                                    cp.abs(cp.dot(X, self.w1) + self.b1)))
-            
-        
-            
-            # Assign data points to class +1 or -1 based on distance from hyperplanes
-            output = 2 * cp.argmin(dist, axis=1) - 1
-    
-            return cp.asnumpy(output)
-
-except ImportError:
-            
-    print("Cannot run GPU implementation. Install CuPy package.")
-###############################################################################
+#
+#try:
+#
+#    from cupy import prof
+#    import cupy as cp
+#    
+#    def GPU_rbf_kernel(x, y, u):
+#        """
+#        It transforms samples into higher dimension using Gaussian (RBF) kernel.
+#    
+#        Parameters
+#        ----------
+#        x, y : array-like, shape (n_features,)
+#            A feature vector or sample.
+#    
+#        u : float
+#            Parameter of the RBF kernel function.
+#    
+#        Returns
+#        -------
+#        float
+#            Value of kernel matrix for feature vector x and y.
+#        """
+#    
+#        return cp.exp(-2 * u) * cp.exp(2 * u * cp.dot(x, y))
+#        
+#    class GPU_LSTSVM():
+#        """
+#        GPU implementation of least squares twin support vector machine
+#        
+#        Parameters
+#        ----------
+#        kernel : str 
+#            Type of the kernel function which is either 'linear' or 'RBF'.
+#    
+#        rect_kernel : float
+#            Percentage of training samples for Rectangular kernel.
+#    
+#        C1 : float
+#            Penalty parameter of first optimization problem.
+#    
+#        C2 : float
+#            Penalty parameter of second optimization problem.
+#    
+#        gamma : float
+#            Parameter of the RBF kernel function.
+#    
+#        Attributes
+#        ----------
+#        mat_C_t : array-like, shape = [n_samples, n_samples]
+#            A matrix that contains kernel values.
+#    
+#        cls_name : str
+#            Name of the classifier.
+#    
+#        w1 : array-like, shape=[n_features]
+#            Weight vector of class +1's hyperplane.
+#    
+#        b1 : float
+#            Bias of class +1's hyperplane.
+#    
+#        w2 : array-like, shape=[n_features]
+#            Weight vector of class -1's hyperplane.
+#    
+#        b2 : float
+#            Bias of class -1's hyperplane.
+#        """
+#        
+#        def __init__(self, kernel='linear', rect_kernel=1, C1=2**0, C2=2**0,
+#                     gamma=2**0):
+#            
+#            self.C1 = C1
+#            self.C2 = C2
+#            self.gamma = gamma
+#            self.kernel = kernel
+#            self.rect_kernel = rect_kernel
+#            self.mat_C_t = None
+#            self.clf_name = None
+#            
+#            # Two hyperplanes attributes
+#            self.w1, self.b1, self.w2, self.b2 = None, None, None, None
+#        
+#        #@prof.TimeRangeDecorator()
+#        def fit(self, X, y):
+#            """
+#            It fits the binary Least Squares TwinSVM model according to the given
+#            training data.
+#    
+#            Parameters
+#            ----------
+#            X : array-like, shape (n_samples, n_features)
+#                Training feature vectors, where n_samples is the number of samples
+#                and n_features is the number of features.
+#    
+#            y : array-like, shape(n_samples,)
+#                Target values or class labels.
+#            """
+#            
+#            X = cp.asarray(X)
+#            y = cp.asarray(y)
+#            
+#            # Matrix A or class 1 data
+#            mat_A = X[y == 1]
+#            
+#            # Matrix B or class -1 data
+#            mat_B = X[y == -1]
+#            
+#            # Vectors of ones
+#            mat_e1 = cp.ones((mat_A.shape[0], 1))
+#            mat_e2 = cp.ones((mat_B.shape[0], 1))
+#            
+#            mat_H = cp.column_stack((mat_A, mat_e1))
+#            mat_G = cp.column_stack((mat_B, mat_e2))
+#    
+#            mat_H_t = cp.transpose(mat_H)
+#            mat_G_t = cp.transpose(mat_G)
+#            
+#            # Determine parameters of two non-parallel hyperplanes
+#            hyper_p_1 = -1 * cp.dot(cp.linalg.inv(cp.dot(mat_G_t, mat_G) + \
+#                                   (1 / self.C1) * cp.dot(mat_H_t, mat_H)), \
+#                                    cp.dot(mat_G_t, mat_e2))
+#            
+#            self.w1 = hyper_p_1[:hyper_p_1.shape[0] - 1, :]
+#            self.b1 = hyper_p_1[-1, :]
+#            
+#            hyper_p_2 = cp.dot(cp.linalg.inv(cp.dot(mat_H_t, mat_H) + (1 / self.C2)
+#                                                 * cp.dot(mat_G_t, mat_G)), cp.dot(mat_H_t, mat_e1))
+#    
+#            self.w2 = hyper_p_2[:hyper_p_2.shape[0] - 1, :]
+#            self.b2 = hyper_p_2[-1, :]
+#        
+#        #@prof.TimeRangeDecorator()
+#        def predict(self, X):
+#            """
+#            Performs classification on samples in X using the Least Squares 
+#            TwinSVM model.
+#            
+#            Parameters
+#            ----------
+#            X_test : array-like, shape (n_samples, n_features)
+#                Feature vectors of test data.
+#                    
+#            Returns
+#            -------
+#            output : array, shape (n_samples,)
+#                Predicted class lables of test data.
+#                
+#            """
+#            
+#            X = cp.asarray(X)
+#    
+#            # Calculate prependicular distances for new data points 
+#    #        prepen_distance = cp.zeros((X.shape[0], 2))
+#    #
+#    #        kernel_f = {'linear': lambda i: X[i, :] ,
+#    #                    'RBF': lambda i: GPU_rbf_kernel(X[i, :], self.mat_C_t, self.gamma)}
+#    #
+#    #        for i in range(X.shape[0]):
+#    #
+#    #            # Prependicular distance of data pint i from hyperplanes
+#    #            prepen_distance[i, 1] = cp.abs(cp.dot(kernel_f[self.kernel](i), self.w1) + self.b1)[0]
+#    #        
+#    #            prepen_distance[i, 0] = cp.abs(cp.dot(kernel_f[self.kernel](i), self.w2) + self.b2)[0]
+#            
+#            dist = cp.column_stack((cp.abs(cp.dot(X, self.w2) + self.b2), 
+#                                    cp.abs(cp.dot(X, self.w1) + self.b1)))
+#            
+#        
+#            
+#            # Assign data points to class +1 or -1 based on distance from hyperplanes
+#            output = 2 * cp.argmin(dist, axis=1) - 1
+#    
+#            return cp.asnumpy(output)
+#
+#except ImportError:
+#            
+#    print("Cannot run GPU implementation. Install CuPy package.")
+################################################################################
 
 
 if __name__ == '__main__':
 
-    from preprocess import read_data
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import accuracy_score
-
-    X, y, filename = read_data('../dataset/australian.csv')
-
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    
-    tsvm_model = TSVM('linear', 0.25, 0.5)
-    
-    tsvm_model.fit(x_train, y_train)
-    pred = tsvm_model.predict(x_test)
-    
-    print("Accuracy: %.2f" % (accuracy_score(y_test, pred) * 100))
+    pass
+#    from preprocess import read_data
+#    from sklearn.model_selection import train_test_split
+#    from sklearn.metrics import accuracy_score
+#
+#    X, y, filename = read_data('../dataset/australian.csv')
+#
+#    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+#    
+#    tsvm_model = TSVM('linear', 0.25, 0.5)
+#    
+#    tsvm_model.fit(x_train, y_train)
+#    pred = tsvm_model.predict(x_test)
+#    
+#    print("Accuracy: %.2f" % (accuracy_score(y_test, pred) * 100))
