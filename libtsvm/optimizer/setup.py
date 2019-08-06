1b2da06c78eb2131af579861af28c0dae719cfa6
mir-am <mir-am@hotmail.com>
2019-08-06 22:06:36 +0430

    Cleaned setup.py files.

--- libtsvm/optimizer/setup.py
+++ libtsvm/optimizer/setup.py
@@ -27,14 +27,15 @@ def check_libs_exist():
     It checks whether required libraries exists on user's system for compiling
     the C++ extension module.
     """
-    
+
     if not (find_library('lapack') and find_library('blas')):
-        
+
         print("Could not find the required libraries. Please use your "
               "distribution package manager to install LAPACK and BLAS "
               "libraries. Otherwise, C++ extension cannot be generated.")
-        
+
         exit()
+
 
 libs = []
 link_args = []
@@ -42,21 +43,22 @@ compile_args = ['-std=c++11', '-DARMA_DONT_USE_WRAPPER']
 
 # Choose appropriate libraries depeneding on the OS
 if platform == 'win32':
-    
+
     libs = ['lapack_win64_MT', 'blas_win64_MT']
     link_args = link_args + ['/MT']
 
 elif platform == 'linux':
-    
+
     check_libs_exist()
-    
+
     libs = ['blas', 'lapack']
-    
+
 elif platform == 'darwin':
     # TODO: Add libs for OSX.
-    
-    #link_args = ['-framework Accelerate']
-    compile_args = compile_args + ['-stdlib=libc++', '-mmacosx-version-min=10.9']
+
+    # link_args = ['-framework Accelerate']
+    compile_args = compile_args + ['-stdlib=libc++',
+                                   '-mmacosx-version-min=10.9']
 
 
 #ext_clipdcd = Extension("clipdcd",
@@ -70,12 +72,12 @@ elif platform == 'darwin':
 
 def configuration(parent_package='', top_path=None):
     """A setup config for building clipdcd extension module """
-    
+
     from numpy.distutils.misc_util import Configuration
     from Cython.Build import cythonize
-    
+
     config = Configuration('optimizer', parent_package, top_path)
-    
+
     # TODO: Check whether compile and link flags for both library and extension
     # is needed.
     config.add_library('clipdcd-lib',
@@ -87,10 +89,10 @@ def configuration(parent_package='', top_path=None):
                         extra_compiler_args=compile_args,
                      )
             
-    
+
     sources = [join("src", "clipdcd.pyx")]
     depends = [join("src", "clippdcd_opt.cpp"), join("src", "clippdcd_opt.h")]
-    
+
     config.add_extension('clipdcd',
                          sources=sources,
                          depends=depends,
@@ -101,32 +103,14 @@ def configuration(parent_package='', top_path=None):
                          extra_link_args=link_args,
                          include_dirs=[np.get_include(), join('armadillo-code',
                                        'include')])
-    
+
     config.ext_modules = cythonize(config.ext_modules)
-    
+
     return config
 
+
 if __name__ == '__main__':
-    
+
     from numpy.distutils.core import setup
-    
+
     setup(**configuration(top_path='').todict())
-    
-    
-#setup(name='clipdcd',
-#      version='0.2.0',
-#      author='Mir, A.',
-#      author_email='mir-am@hotmail.com',
-#      url='https://github.com/mir-am/LightTwinSVM',
-#      description='clipDCD opimtizer implemented in C++ and improved by Mir, A.',
-#      ext_modules=cythonize(Extension(
-#        "clipdcd",
-#        sources=[join("src", "clipdcd.pyx"), join("src", "clippdcd_opt.cpp")],
-#        language="c++",
-#        libraries=libs,
-#        library_dirs=['.\\armadillo-code\\lib_win64'],
-#        extra_compile_args=compile_args,
-#        extra_link_args=link_args,
-#        include_dirs=[np.get_include(), './armadillo-code/include']
-#        )))
-      
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Installation of LIBTwinSVM library.
 from libtsvm import __version__
 from pkg_resources import parse_version
 from shutil import copy
-#from setuptools import find_packages, setup, Command
+# from setuptools import find_packages, setup, Command
 import os
 import subprocess
 import sys
@@ -27,8 +27,8 @@ EMAIL = "mir-am@hotmail.com, mahdirahbar@gmail.com"
 AUTHOR = "Mir, A. and Mahdi Rahbar"
 REQUIRES_PYTHON = '>=3.5'
 VERSION = '%s' % __version__
-REQ_PACKAGES = ["cython", "numpy", "matplotlib", "pyQt5", "sklearn","pandas",
-            "xlsxwriter", "joblib","numpydoc==0.7.0"]
+REQ_PACKAGES = ["cython", "numpy", "matplotlib", "pyQt5", "sklearn", "pandas",
+                "xlsxwriter", "joblib", "numpydoc==0.7.0"]
 PKG_DATA = {}
 DATA_FILES = []
 
@@ -41,60 +41,6 @@ LONG_DESCRIPTION = ("Please check out the project's `GitHub page "
                     "<https://github.com/mir-am/LIBTwinSVM>`_ for installation"
                     " guide and documentation.")
 
-# Load the package's __version__.py module as a dictionary.
-#about = {}
-#if not VERSION:
-#    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-#    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-#        exec(f.read(), about)
-#else:
-#    about['__version__'] = VERSION
-
-
-#def build_ext_clipdcd():
-#    """It builds the clipdcd extension module"""
-#    
-#    from libtsvm.optimizer.setup import ext_clipdcd
-#    from Cython.Build import cythonize    
-#    
-#    cythonize(ext_clipdcd)
-
-# class UploadCommand(Command):
-#     """Support setup.py upload."""
-
-#     description = 'Build and publish the package.'
-#     user_options = []
-
-#     @staticmethod
-#     def status(s):
-#         """Prints things in bold."""
-#         print('\033[1m{0}\033[0m'.format(s))
-
-#     def initialize_options(self):
-#         pass
-
-#     def finalize_options(self):
-#         pass
-
-#     def run(self):
-#         try:
-#             self.status('Removing previous builds…')
-#             rmtree(os.path.join(here, 'dist'))
-#         except OSError:
-#             pass
-
-#         self.status('Building Source and Wheel (universal) distribution…')
-#         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-#         self.status('Uploading the package to PyPI via Twine…')
-#         os.system('twine upload dist/*')
-
-#         self.status('Pushing git tags…')
-#         os.system('git tag v{0}'.format(about['__version__']))
-#         os.system('git push --tags')
-        
-#         sys.exit()
-    
 # using setuptool features
 SETUPTOOLS_COMMANDS = set([
     'develop', 'release', 'install', 'bdist_egg', 'bdist_rpm',
@@ -104,20 +50,22 @@ SETUPTOOLS_COMMANDS = set([
 ])
 if SETUPTOOLS_COMMANDS.intersection(sys.argv):
     import setuptools
-    
+
     class BinaryDistribution(setuptools.Distribution):
         def has_ext_modules(foo):
             return True
-        
+
     if 'bdist_wheel' in sys.argv or 'install' in sys.argv:
-        
+
         if sys.platform == 'win32':
-        
+
             DATA_FILES = [(os.path.join('libtsvm', 'optimizer'),
-            [os.path.join('libtsvm', 'optimizer', 'lapack_win64_MT.dll'),
+                         [os.path.join('libtsvm', 'optimizer',
+                                       'lapack_win64_MT.dll'),
              os.path.join('libtsvm', 'optimizer', 'blas_win64_MT.dll')])]
-        
-            print("Added LAPACK and BLAS DLLs to the wheel for Windows platform.")
+
+            print("Added LAPACK and BLAS DLLs to the wheel for Windows "
+                  "platform.")
 
     extra_setuptools_args = dict(
         zip_safe=False,  # the package can run out of an .egg file
@@ -131,79 +79,79 @@ if SETUPTOOLS_COMMANDS.intersection(sys.argv):
             ),
         },
     )
-        
-    
-        
+
 else:
     extra_setuptools_args = dict()
 
-    
+
 ### Utility functinos for installation #######################################    
 def cp_libs_win():
     """
     Copies external libraries for installing package on Windows.
     """
-    
+
     src_1 = os.path.join('libtsvm', 'optimizer', 'armadillo-code', 'lib_win64',
                          'lapack_win64_MT.dll')
     src_2 = os.path.join('libtsvm', 'optimizer', 'armadillo-code', 'lib_win64',
                          'blas_win64_MT.dll')
-    
+
     dst = os.path.join('libtsvm', 'optimizer')
-    
+
     copy(src_1, dst)
     copy(src_2, dst)
-    
+
     print("Copied the LAPACK and BLAS libraries to source distribution...")
 
-    
+
 def get_numpy_status():
     """
-    It checks whether numpy is installed. If so, checks whether it is up-to-date.
+    It checks whether numpy is installed. If so, checks whether it is
+    up-to-date.
     """
-    
+
     np_status = {}
-    
+
     try:
-        
+
         import numpy
-        
+
         np_status['version'] = numpy.__version__
         np_status['up_to_date'] = parse_version(np_status['version']) >=  \
         parse_version(NUMPY_MIN_VERSION)
-        
+
     except ImportError:
-        
+
         traceback.print_exc()
-        
+
         np_status['version'] = ""
         np_status['up_to_date'] = False
-        
+
     return np_status
 
 
 def get_cython_status():
     """
-    It checks whether Cython is installed. If so, checks whether it is up-to-date.
+    It checks whether Cython is installed. If so, checks whether it is
+    up-to-date.
     """
-    
+
     cy_status = {}
-    
+
     try:
-        
+
         import cython
-        
+
         cy_status['version'] = cython.__version__
         cy_status['up_to_date'] = parse_version(cy_status['version']) >=  \
         parse_version(CYTHON_MIN_VERSION)
-        
+
     except ImportError:
-        
+
         traceback.print_exc()
-        
+
         cy_status['version'] = ""
         cy_status['up_to_date'] = False
-        
+
     return cy_status
 
 
@@ -211,24 +159,24 @@ def check_install_updated(pkg_name, pkg_status, req_str, help_instr):
     """
     It checks whether a package is installed and up-to-date.
     """
-    
+
     if pkg_status['version']:
-        
+
         if pkg_status['up_to_date'] is False:
-            
+
             raise ImportError("Your current version of %s %s is out-of-date. %s %s" %  \
                               (pkg_name, pkg_status['version'], req_str,
                                help_instr))
-            
+
         else:
-            
+
             print("Found %s %s" % (pkg_name, pkg_status['version']))
-        
+
     else:
-        
+
         raise ImportError("%s is not installed. %s %s "
                           % (pkg_name, req_str, help_instr))
-    
+
 
 # This function is written with inspiration from NumPy's setup.py
 def check_arma_submodule():
@@ -237,122 +185,86 @@ def check_arma_submodule():
     """
 
     if not os.path.exists('.git'):
-        return # Not a git reposiroty
-    
+        return  # Not a git reposiroty
+
     help_instr = ("Please use the following command to initialize Armadillo"
                   " submodule:\ngit submodule update --init")
-    
+
     # Check submodule is not missing
     with open('.gitmodules') as f:
         for l in f:
             if 'path' in l:
                 p = l.split('=')[-1].strip()
                 if not os.path.exists(p):
-                    raise ValueError('Submodule %s missing.\n%s' % (p, help_instr))
-    
-    # Check submodule is clean                
+                    raise ValueError('Submodule %s missing.\n%s' % (p,
+                                     help_instr))
+
+    # Check submodule is clean
     proc = subprocess.Popen(['git', 'submodule', 'status'],
                             stdout=subprocess.PIPE)
     status, _ = proc.communicate()
     status = status.decode("ascii", "replace")
     for line in status.splitlines():
         if line.startswith('-') or line.startswith('+'):
-            raise ValueError('Submodule not clean: %s\n%s' % (line, help_instr))
-    
+            raise ValueError('Submodule not clean: %s\n%s' % (line,
+                                                              help_instr))
+
     print("Found Armadillo submodule.")
-    
-    
+
+
 def detect_anaconda_dist():
     """
     It detects Anaconda distribution.
     """
-    
+
     return True if 'anaconda' in sys.version.lower() else False
-        
-# This code is borrowed from Matthew Brett's windows-wheel-builder
-#def add_dll_wheel(dist_path='dist'):
-#    """
-#    Adds runtime DLLs to pre-built wheels for Windows.
-#    """
-#    
-#    from glob import glob
-#    # delocate package is only needed for building wheels for Windows systems.
-#    from delocate import wheeltools
-#    from zipfile import ZipFile
-#    
-#    def my_zip2dir(zip_fname, out_dir):
-#        """ Removes need for 'unzip' binary in PATH during `wheeltools.zip2dir`
-#        """
-#        with open(zip_fname, 'rb') as fobj:
-#            zip = ZipFile(fobj)
-#            zip.extractall(path=out_dir)
-#
-#    # Monkeypatch wheeltools
-#    wheeltools.zip2dir = my_zip2dir
-#    
-#    lapack_dll = os.path.join('libtsvm', 'optimizer', 'lapack_win64_MT.dll')
-#    blas_dll = os.path.join('libtsvm', 'optimizer', 'blas_win64_MT.dll')
-#    
-#    wheel_fnames = glob(os.path.join(dist_path, '*.whl'))
-#    
-#    print(wheel_fnames)
-#    
-#    for fname in wheel_fnames:
-#        print('Processing', fname)
-#        with wheeltools.InWheel(fname, fname):
-#            print(os.path.abspath())
-#            
-#            copy2(lapack_dll, os.path.join('libtsvm', 'optimizer'))
-#            copy2(blas_dll, os.path.join('libtsvm', 'optimizer'))
-#            
-#    print("Added LAPACK and BLAS DLLs to the wheel.")
-    
-##############################################################################
-    
+
+
 def configuration(parent_package='', top_path=None):
-    
+
     from numpy.distutils.misc_util import Configuration
-    
+
     config = Configuration(None, parent_package, top_path)
-    
+
     config.set_options(ignore_setup_xxx_py=True,
                        assume_default_configuration=True,
-                       delegate_options_to_subpackages=True) #,
-                       #quiet=True)
-                       
+                       delegate_options_to_subpackages=True)
+                       # quiet=True)
+
     config.add_subpackage('libtsvm')
-    
+
     return config
 
+
 def setup_package():
-    
+
     global PKG_DATA
-    
+
     # Check dependencies and requirements ####################################
     help_instr = ("Please check out the installation guide of the LIBTwinSVM:\n"
                   "https://libtwinsvm.readthedocs.io/en/latest/")
     np_req_str = "LIBTwinSVM requires NumPy >= %s.\n" % NUMPY_MIN_VERSION
     cy_req_str = ("Please install cython with a version >= %s in order to"
                   " build the LIBTwinSVM library." % CYTHON_MIN_VERSION)
-    
+
     check_arma_submodule()
-    
+
     # Check numpy status and its version on users' system
     np_status = get_numpy_status()
     cy_status = get_cython_status()
-    
+
     check_install_updated('Numerical Python (NumPy)', np_status, np_req_str,
                           help_instr)
     check_install_updated('Cython', cy_status, cy_req_str, help_instr)
     ##########################################################################
-    
+
     # Platform-dependent options
     if sys.platform == 'win32':
-        
+
         cp_libs_win()
-        
+
         PKG_DATA = {'libtsvm': [os.path.join('libtsvm', 'optimizer', '*.dll')]}
-        
+
     # Remove PyQT5 from dependencies for Anaconda distribution
     if detect_anaconda_dist():
         # TODO: Check whether PyQT is installed.
@@ -373,7 +285,7 @@ def setup_package():
         #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
         # If your package is a single module, use this instead of 'packages':
         # py_modules=['mypackage'],
-    
+
         # entry_points={
         #     'console_scripts': ['mycli=mymodule:cli'],
         # },
@@ -398,14 +310,13 @@ def setup_package():
             'Topic :: Scientific/Engineering :: Artificial Intelligence'
         ],
         **extra_setuptools_args)
-    
+
     from numpy.distutils.core import setup
-    
+
     metadata['configuration'] = configuration
-    
+
     setup(**metadata)
-    
-    
+
+
 if __name__ == '__main__':
     setup_package()
-    
