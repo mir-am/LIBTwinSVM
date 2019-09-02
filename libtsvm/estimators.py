d8c3989f8a31ab16e6e6b738338906b58a2382ad
mir-am <mir-am@hotmail.com>
2019-09-02 15:21:22 +0430

    A simpler solution, fit() methods of TSVM-based estimators now accept list data.

--- libtsvm/estimators.py
+++ libtsvm/estimators.py
@@ -202,7 +202,8 @@ class TSVM(BaseTSVM):
         super(TSVM, self).__init__(kernel, rect_kernel, C1, C2, gamma)
 
         self.clf_name = 'TSVM'
-    #@profile
+
+    # @profile
     def fit(self, X_train, y_train):
         """
         It fits the binary TwinSVM model according to the given training data.
@@ -218,7 +219,9 @@ class TSVM(BaseTSVM):
 
         """
 
-        X_train, y_train = check_X_y(X_train, y_train, dtype=np.float64)
+        X_train = np.array(X_train, dtype=np.float64) if isinstance(X_train,
+                          list) else X_train
+        y_train = np.array(y_train) if isinstance(y_train, list) else y_train
 
         # Matrix A or class 1 samples
         mat_A = X_train[y_train == 1]
@@ -258,18 +261,18 @@ class TSVM(BaseTSVM):
 
         mat_H_H = np.linalg.inv(np.dot(mat_H_t, mat_H) + (reg_term *
                                 np.identity(mat_H.shape[1])))
-        
+
         # Wolfe dual problem of class 1
         mat_dual1 = np.dot(np.dot(mat_G, mat_H_H), mat_G_t)
         # Obtaining Lagrange multipliers using ClipDCD optimizer
         alpha_d1 = clipdcd.optimize(mat_dual1, self.C1).reshape(mat_dual1.shape[0], 1)
-        
+
         # Obtain hyperplanes
         hyper_p_1 = -1 * np.dot(np.dot(mat_H_H, mat_G_t), alpha_d1)
-        
+
         # Free memory
         del mat_dual1, mat_H_H
-        
+
         mat_G_G = np.linalg.inv(np.dot(mat_G_t, mat_G) + (reg_term *
                                 np.identity(mat_G.shape[1])))
         # Wolfe dual problem of class -1
@@ -308,7 +311,7 @@ class LSTSVM(BaseTSVM):
 
     gamma : float, optional (default=1.0)
         Parameter of the RBF kernel function.
-        
+
     mem_optimize : boolean, optional (default=False)
         If it's True, it optimizes the memory consumption siginificantly.
         However, the memory optimization increases the CPU time.
@@ -318,10 +321,11 @@ class LSTSVM(BaseTSVM):
                  gamma=2**0, mem_optimize=False):
 
         super(LSTSVM, self).__init__(kernel, rect_kernel, C1, C2, gamma)
-        
+
         self.mem_optimize = mem_optimize
         self.clf_name = 'LSTSVM'
-    #@profile
+
+    # @profile
     def fit(self, X, y):
         """
         It fits the binary Least Squares TwinSVM model according to the given
@@ -337,7 +341,8 @@ class LSTSVM(BaseTSVM):
             Target values or class labels.
         """
 
-        #X, y = check_X_y(X, y, dtype=np.float64)
+        X = np.array(X, dtype=np.float64) if isinstance(X, list) else X
+        y = np.array(y) if isinstance(y, list) else y
 
         # Matrix A or class 1 data
         mat_A = X[y == 1]
@@ -356,7 +361,7 @@ class LSTSVM(BaseTSVM):
 
             mat_H = np.column_stack((mat_A, mat_e1))
             mat_G = np.column_stack((mat_B, mat_e2))
-    
+
         elif self.kernel == 'RBF':
 
             # class 1 & class -1
@@ -373,178 +378,177 @@ class LSTSVM(BaseTSVM):
 
         mat_H_t = np.transpose(mat_H)
         mat_G_t = np.transpose(mat_G)
-        
+
         if self.mem_optimize:
-            
+
             inv_p_1 = np.linalg.inv((np.dot(mat_G_t, mat_G) + (1 / self.C1) \
                       * np.dot(mat_H_t,mat_H)) + (reg_term * np.identity(mat_H.shape[1])))
-            
-    
+
             # Determine parameters of two non-parallel hyperplanes
             hyper_p_1 = -1 * np.dot(inv_p_1, np.dot(mat_G_t, mat_e2))
-            
+
             # Free memory
             del inv_p_1
-            
+
             inv_p_2 = np.linalg.inv((np.dot(mat_H_t,mat_H) + (1 / self.C2) \
                       * np.dot(mat_G_t, mat_G)) + (reg_term * np.identity(mat_H.shape[1])))
 
             hyper_p_2 = np.dot(inv_p_2, np.dot(mat_H_t, mat_e1))
-            
+
         else:
-        
+
             stabilizer = reg_term * np.identity(mat_H.shape[1])
-            
+
             mat_G_G_t = np.dot(mat_G_t, mat_G)
             mat_H_H_t = np.dot(mat_H_t,mat_H)
-            
+
             inv_p_1 = np.linalg.inv((mat_G_G_t + (1 / self.C1) * mat_H_H_t) \
                                     + stabilizer)
             # Determine parameters of two non-parallel hyperplanes
             hyper_p_1 = -1 * np.dot(inv_p_1, np.dot(mat_G_t, mat_e2))
-            
+
             # Free memory
             del inv_p_1
-            
+
             inv_p_2 = np.linalg.inv((mat_H_H_t + (1 / self.C2) * mat_G_G_t) \
                                     + stabilizer)
 
             hyper_p_2 = np.dot(inv_p_2, np.dot(mat_H_t, mat_e1))
-            
+
         self.w1 = hyper_p_1[:hyper_p_1.shape[0] - 1, :]
         self.b1 = hyper_p_1[-1, :]
-        
+
         self.w2 = hyper_p_2[:hyper_p_2.shape[0] - 1, :]
         self.b2 = hyper_p_2[-1, :]
-    
+
 #    @profile
-    def fit_2(self, X, y):
-        """
-        It fits the binary Least Squares TwinSVM model according to the given
-        training data.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            Training feature vectors, where n_samples is the number of samples
-            and n_features is the number of features.
-
-        y : array-like, shape(n_samples,)
-            Target values or class labels.
-        """
-
-        X, y = check_X_y(X, y, dtype=np.float64)
-
-        # Matrix A or class 1 data
-        mat_A = X[y == 1]
-
-        # Matrix B or class -1 data
-        mat_B = X[y == -1]
-
-        # Vectors of ones
-        mat_e1 = np.ones((mat_A.shape[0], 1))
-        mat_e2 = np.ones((mat_B.shape[0], 1))
-
-        # Regularization term used for ill-possible condition
-        reg_term = 2 ** float(-7)
-
-        if self.kernel == 'linear':
-
-            mat_H = np.column_stack((mat_A, mat_e1))
-            mat_G = np.column_stack((mat_B, mat_e2))
-
-            mat_H_t = np.transpose(mat_H)
-            mat_G_t = np.transpose(mat_G)
-
-            stabilizer = reg_term * np.identity(mat_H.shape[1])
-
-            # Determine parameters of two non-parallel hyperplanes
-            hyper_p_1 = -1 * np.dot(np.linalg.inv((np.dot(mat_G_t, mat_G) +
-                        (1 / self.C1) * np.dot(mat_H_t,mat_H)) + stabilizer),
-                        np.dot(mat_G_t, mat_e2))
-
-            self.w1 = hyper_p_1[:hyper_p_1.shape[0] - 1, :]
-            self.b1 = hyper_p_1[-1, :]
-
-            hyper_p_2 = np.dot(np.linalg.inv((np.dot(mat_H_t, mat_H) + (1 / self.C2)
-                        * np.dot(mat_G_t, mat_G)) + stabilizer), np.dot(mat_H_t, mat_e1))
-
-            self.w2 = hyper_p_2[:hyper_p_2.shape[0] - 1, :]
-            self.b2 = hyper_p_2[-1, :]
-
-        elif self.kernel == 'RBF':
-
-            # class 1 & class -1
-            mat_C = np.row_stack((mat_A, mat_B))
-
-            self.mat_C_t = np.transpose(mat_C)[:, :int(mat_C.shape[0] *
-                                               self.rect_kernel)]
-
-            mat_H = np.column_stack((rbf_kernel(mat_A, self.mat_C_t,
-                                                self.gamma), mat_e1))
-
-            mat_G = np.column_stack((rbf_kernel(mat_B, self.mat_C_t,
-                                                self.gamma), mat_e2))
-
-            mat_H_t = np.transpose(mat_H)
-            mat_G_t = np.transpose(mat_G)
-
-            mat_I_H = np.identity(mat_H.shape[0])  # (m_1 x m_1)
-            mat_I_G = np.identity(mat_G.shape[0])  # (m_2 x m_2)
-
-            mat_I = np.identity(mat_G.shape[1])  # (n x n)
-
-            # Determine parameters of hypersurfaces # Using SMW formula
-            if mat_A.shape[0] < mat_B.shape[0]:
-
-                y = (1 / reg_term) * (mat_I - np.dot(np.dot(mat_G_t, \
-                    np.linalg.inv((reg_term * mat_I_G) + np.dot(mat_G, mat_G_t))),
-                                                                mat_G))
-
-                mat_H_y = np.dot(mat_H, y)
-                mat_y_Ht = np.dot(y, mat_H_t)
-                mat_H_y_Ht = np.dot(mat_H_y, mat_H_t)
-                
-                h_surf1_inv = np.linalg.inv(self.C1 * mat_I_H + mat_H_y_Ht)
-                h_surf2_inv = np.linalg.inv((mat_I_H / self.C2) + mat_H_y_Ht)
-
-                hyper_surf1 = np.dot(-1 * (y - np.dot(np.dot(mat_y_Ht, h_surf1_inv),
-                                                      mat_H_y)), np.dot(mat_G_t, mat_e2))
-
-                hyper_surf2 = np.dot(self.C2 * (y - np.dot(np.dot(mat_y_Ht,
-                                    h_surf2_inv), mat_H_y)), np.dot(mat_H_t, mat_e1))
-
-                # Parameters of hypersurfaces
-                self.w1 = hyper_surf1[:hyper_surf1.shape[0] - 1, :]
-                self.b1 = hyper_surf1[-1, :]
-
-                self.w2 = hyper_surf2[:hyper_surf2.shape[0] - 1, :]
-                self.b2 = hyper_surf2[-1, :]
-
-            else:
-
-                z = (1 / reg_term) * (mat_I - np.dot(np.dot(mat_H_t, \
-                    np.linalg.inv(reg_term * mat_I_H + np.dot(mat_H, mat_H_t))),
-                    mat_H))
-                    
-                mat_G_z = np.dot(mat_G, z)
-                mat_z_Gt = np.dot(z, mat_G_t)
-                mat_G_y_Gt = np.dot(mat_G_z, mat_G_t)
-
-                g_surf1_inv = np.linalg.inv((mat_I_G / self.C1) + mat_G_y_Gt)
-                g_surf2_inv = np.linalg.inv(self.C2 * mat_I_G + mat_G_y_Gt)
-
-                hyper_surf1 = np.dot(-self.C1 * (z - np.dot(np.dot(mat_z_Gt,
-                                    g_surf1_inv), mat_G_z)), np.dot(mat_G_t, mat_e2))
-
-                hyper_surf2 = np.dot((z - np.dot(np.dot(mat_z_Gt, g_surf2_inv),
-                                    mat_G_z)), np.dot(mat_H_t, mat_e1))
-
-                self.w1 = hyper_surf1[:hyper_surf1.shape[0] - 1, :]
-                self.b1 = hyper_surf1[-1, :]
-
-                self.w2 = hyper_surf2[:hyper_surf2.shape[0] - 1, :]
-                self.b2 = hyper_surf2[-1, :]
+#    def fit_2(self, X, y):
+#        """
+#        It fits the binary Least Squares TwinSVM model according to the given
+#        training data.
+#
+#        Parameters
+#        ----------
+#        X : array-like, shape (n_samples, n_features)
+#            Training feature vectors, where n_samples is the number of samples
+#            and n_features is the number of features.
+#
+#        y : array-like, shape(n_samples,)
+#            Target values or class labels.
+#        """
+#
+#        X, y = check_X_y(X, y, dtype=np.float64)
+#
+#        # Matrix A or class 1 data
+#        mat_A = X[y == 1]
+#
+#        # Matrix B or class -1 data
+#        mat_B = X[y == -1]
+#
+#        # Vectors of ones
+#        mat_e1 = np.ones((mat_A.shape[0], 1))
+#        mat_e2 = np.ones((mat_B.shape[0], 1))
+#
+#        # Regularization term used for ill-possible condition
+#        reg_term = 2 ** float(-7)
+#
+#        if self.kernel == 'linear':
+#
+#            mat_H = np.column_stack((mat_A, mat_e1))
+#            mat_G = np.column_stack((mat_B, mat_e2))
+#
+#            mat_H_t = np.transpose(mat_H)
+#            mat_G_t = np.transpose(mat_G)
+#
+#            stabilizer = reg_term * np.identity(mat_H.shape[1])
+#
+#            # Determine parameters of two non-parallel hyperplanes
+#            hyper_p_1 = -1 * np.dot(np.linalg.inv((np.dot(mat_G_t, mat_G) +
+#                        (1 / self.C1) * np.dot(mat_H_t,mat_H)) + stabilizer),
+#                        np.dot(mat_G_t, mat_e2))
+#
+#            self.w1 = hyper_p_1[:hyper_p_1.shape[0] - 1, :]
+#            self.b1 = hyper_p_1[-1, :]
+#
+#            hyper_p_2 = np.dot(np.linalg.inv((np.dot(mat_H_t, mat_H) + (1 / self.C2)
+#                        * np.dot(mat_G_t, mat_G)) + stabilizer), np.dot(mat_H_t, mat_e1))
+#
+#            self.w2 = hyper_p_2[:hyper_p_2.shape[0] - 1, :]
+#            self.b2 = hyper_p_2[-1, :]
+#
+#        elif self.kernel == 'RBF':
+#
+#            # class 1 & class -1
+#            mat_C = np.row_stack((mat_A, mat_B))
+#
+#            self.mat_C_t = np.transpose(mat_C)[:, :int(mat_C.shape[0] *
+#                                               self.rect_kernel)]
+#
+#            mat_H = np.column_stack((rbf_kernel(mat_A, self.mat_C_t,
+#                                                self.gamma), mat_e1))
+#
+#            mat_G = np.column_stack((rbf_kernel(mat_B, self.mat_C_t,
+#                                                self.gamma), mat_e2))
+#
+#            mat_H_t = np.transpose(mat_H)
+#            mat_G_t = np.transpose(mat_G)
+#
+#            mat_I_H = np.identity(mat_H.shape[0])  # (m_1 x m_1)
+#            mat_I_G = np.identity(mat_G.shape[0])  # (m_2 x m_2)
+#
+#            mat_I = np.identity(mat_G.shape[1])  # (n x n)
+#
+#            # Determine parameters of hypersurfaces # Using SMW formula
+#            if mat_A.shape[0] < mat_B.shape[0]:
+#
+#                y = (1 / reg_term) * (mat_I - np.dot(np.dot(mat_G_t, \
+#                    np.linalg.inv((reg_term * mat_I_G) + np.dot(mat_G, mat_G_t))),
+#                                                                mat_G))
+#
+#                mat_H_y = np.dot(mat_H, y)
+#                mat_y_Ht = np.dot(y, mat_H_t)
+#                mat_H_y_Ht = np.dot(mat_H_y, mat_H_t)
+#                
+#                h_surf1_inv = np.linalg.inv(self.C1 * mat_I_H + mat_H_y_Ht)
+#                h_surf2_inv = np.linalg.inv((mat_I_H / self.C2) + mat_H_y_Ht)
+#
+#                hyper_surf1 = np.dot(-1 * (y - np.dot(np.dot(mat_y_Ht, h_surf1_inv),
+#                                                      mat_H_y)), np.dot(mat_G_t, mat_e2))
+#
+#                hyper_surf2 = np.dot(self.C2 * (y - np.dot(np.dot(mat_y_Ht,
+#                                    h_surf2_inv), mat_H_y)), np.dot(mat_H_t, mat_e1))
+#
+#                # Parameters of hypersurfaces
+#                self.w1 = hyper_surf1[:hyper_surf1.shape[0] - 1, :]
+#                self.b1 = hyper_surf1[-1, :]
+#
+#                self.w2 = hyper_surf2[:hyper_surf2.shape[0] - 1, :]
+#                self.b2 = hyper_surf2[-1, :]
+#
+#            else:
+#
+#                z = (1 / reg_term) * (mat_I - np.dot(np.dot(mat_H_t, \
+#                    np.linalg.inv(reg_term * mat_I_H + np.dot(mat_H, mat_H_t))),
+#                    mat_H))
+#                    
+#                mat_G_z = np.dot(mat_G, z)
+#                mat_z_Gt = np.dot(z, mat_G_t)
+#                mat_G_y_Gt = np.dot(mat_G_z, mat_G_t)
+#
+#                g_surf1_inv = np.linalg.inv((mat_I_G / self.C1) + mat_G_y_Gt)
+#                g_surf2_inv = np.linalg.inv(self.C2 * mat_I_G + mat_G_y_Gt)
+#
+#                hyper_surf1 = np.dot(-self.C1 * (z - np.dot(np.dot(mat_z_Gt,
+#                                    g_surf1_inv), mat_G_z)), np.dot(mat_G_t, mat_e2))
+#
+#                hyper_surf2 = np.dot((z - np.dot(np.dot(mat_z_Gt, g_surf2_inv),
+#                                    mat_G_z)), np.dot(mat_H_t, mat_e1))
+#
+#                self.w1 = hyper_surf1[:hyper_surf1.shape[0] - 1, :]
+#                self.b1 = hyper_surf1[-1, :]
+#
+#                self.w2 = hyper_surf2[:hyper_surf2.shape[0] - 1, :]
+#                self.b2 = hyper_surf2[-1, :]
 
 
 def rbf_kernel(x, y, u):
